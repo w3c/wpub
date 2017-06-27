67a103d931d6a82cafa1e5a7368370eea48cea48
Matt Garrish <matt.garrish@gmail.com>
2017-06-27 13:29:46 -0400

    minimal spec skeleton with introduction

---
+++ common/js/biblio.js
@@ -0,0 +1,28 @@
+/* 
+ * WARNING: Use this file sparingly! 
+ * 
+ * Check http://www.specref.org/ first
+ * 
+ */
+
+/* general entry template
+   "short_name": {
+      "title": "...",
+      "href": "https://...",
+      "authors": [
+        "..."
+      ],
+      "date": "..."
+   }
+*/
+
+var biblio = {
+   "PackagedWebPubs": {
+      "title": "Packaged Web Publications",
+      "href": "https://w3.org/TR/pkgd-web-pub/",
+      "authors": [
+        "..."
+      ],
+      "date": "..."
+   }
+}
