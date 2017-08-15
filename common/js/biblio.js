165a619d1dcb404b38d52d3626fa40a1dae7149e
Ivan Herman <ivan@ivan-herman.net>
2017-08-15 13:10:42 +0200

    Consolidation of the draft
    * Refer to the publishing and linking document for references (see [resolution](https://www.w3.org/publishing/groups/publ-wg/Meetings/Minutes/2017/2017-08-07-minutes#resolution3))
    * Usage, in preference, of URL-s (see [resolution](https://www.w3.org/publishing/groups/publ-wg/Meetings/Minutes/2017/2017-08-14-minutes#resolution3)). This also include the incorporation (manually) of the changes in PR #31.
    * Reconciling the terminology section, after taking on board PRs #28 and #17
    * Incorporated the content of the [separate document](https://rawgit.com/w3c/wpub/manifest-consensus-proposal/index-manifest-proposal.html#abstract-versus-concrete-manifest) as discussed on the [meeting at 2017-08-14](https://www.w3.org/publishing/groups/publ-wg/Meetings/Minutes/2017/2017-08-14-minutes) (see [resolution](https://www.w3.org/publishing/groups/publ-wg/Meetings/Minutes/2017/2017-08-14-minutes#resolution2)). In doing so, some earlier explanations on identifiers have been removed and/or moved elsewhere, to avoid duplication.
    * Refreshed the list of participants in the acknowledgement section.

--- common/js/biblio.js
+++ common/js/biblio.js
@@ -1,8 +1,8 @@
-/* 
- * WARNING: Use this file sparingly! 
- * 
+/*
+ * WARNING: Use this file sparingly!
+ *
  * Check http://www.specref.org/ first
- * 
+ *
  */
 
 /* general entry template
@@ -17,7 +17,20 @@
 */
 
 var biblio = {
-   "PWPUB": {
+   "link-relation": {
+       "title": "Identifier: A Link Relation to Convey a Preferred URI for Referencing",
+       "authors": [
+           "H. Van de Sompel",
+           "M. Nelson",
+           "G. Bilder",
+           "J. Kunze",
+           "S. Warner"
+       ],
+       "rawDate": "2017-08",
+       "publisher": "IETF",
+       "href": "https://tools.ietf.org/html/draft-vandesompel-identifier-00",
+   },
+   "pwpub": {
       "title": "Packaged Web Publications",
       "href": "https://w3.org/TR/pwpub/",
       "authors": [
@@ -25,7 +38,7 @@ var biblio = {
       ],
       "date": "..."
    },
-   "WPUB": {
+   "wpub": {
       "title": "Web Publications",
       "href": "https://w3.org/TR/wpub/",
       "authors": [
