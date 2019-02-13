49b0f6a1a9e3ba3c5401c45316a0a96863793aef
MattGarrish <mattgarrish@users.noreply.github.com>
2019-02-13 07:28:16 -0400

    add index.html to redirect old draft location; remove pubmanifest to modify later;

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,14 @@
 
-SNAPSHOT = ""
-
 if [ "$TRAVIS_BRANCH" != "publish_webpub"
-      -a "$TRAVIS_BRANCH" != "publish_audiobook"
-      -a "$TRAVIS_BRANCH" != "publish_pubmanifest" ]; then
+      -a "$TRAVIS_BRANCH" != "publish_audiobook" ]; then
     echo " Nothing to be done"
     exit 0
 fi
 
-
-if [ "$TRAVIS_BRANCH" = "publish_webpub" ]; then
-   SNAPSHOT="webpub/snapshot/ECHIDNA"
-fi
+SNAPSHOT="webpub/snapshot/ECHIDNA"
 
 if [ "$TRAVIS_BRANCH" = "publish_audiobook" ]; then
    SNAPSHOT="audiobook/snapshot/ECHIDNA"
-fi
-
-if [ "$TRAVIS_BRANCH" = "publish_pubmanifest" ]; then
-   SNAPSHOT="pubmanifest/snapshot/ECHIDNA"
 fi
 
 test $TRAVIS_PULL_REQUEST = false && curl "https://labs.w3.org/echidna/api/request" --data "url=$URL$SNAPSHOT" --data "decision=$DECISION" --data "token=$TOKEN"
